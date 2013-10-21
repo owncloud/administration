cbe47cb00dd513c2cd44b03c962719af7b447630
Klaas Freitag <freitag@owncloud.com>
2013-10-21 10:41:31 +0200

    Use getopt to handle options, added help text, add numeric mode.
    The numeric mode constructs the word list names from a number and
    a fixed string (ie. file12) rather than picking a random file name
    from the word list module. The random word is obviously much
    nicer, but also much slower which is annoying while testing.

--- davtorture/torture_gen_layout.pl
+++ davtorture/torture_gen_layout.pl
@@ -1,24 +1,48 @@
 #!/usr/bin/env perl
 use strict;
 use Data::Random::WordList;
+use Getopt::Std;
 
 ############################################################################
+
+use vars qw($opt_h $opt_n);
 
 # Which extensions to randomly assign
 my @exts = ('txt', 'pdf', 'html', 'docx', 'xlsx', 'pptx', 'odt', 'ods', 'odp');
 # Maximum depth of the target structure
-my $maxdepth = 6;
+my $maxdepth = 4;
 # Maximum amount of subfolders within a folder
 my $max_subfolders = 6;
 # Maximum amount of files within a folder
-my $max_files_per_folder = 5;
+my $max_files_per_folder = 25;
 # Maximum file size 
 my $max_file_size = 1024**2;
 
 ############################################################################
 
 my $wl; # keep the wordlist global.
+my $cnt = 0;
 my $overall_size;
+
+sub help
+{
+print<<ENDHELP
+
+  torture_gen_layout.pl - generate a torture file list.
+  
+  This script prints a file list to stdout which can be saved and feeded
+  into the torture_create_files.pl script to actually generate the file
+  tree. This makes it easy to create reproducable file trees for tests.
+  
+  Options:
+  -h:        This help text
+  -n:        Use numeric file names rather than random words (faster).
+
+ENDHELP
+;
+
+  exit 1;
+}
 
 sub open_wordlist
 {
@@ -43,11 +67,17 @@ sub gen_entries($)
 {
   my ($count) = @_;
 
-  my @rand_words = $wl->get_words($count);
-  foreach(@rand_words) {
-    $_ =~ s/\'//g;
+  my @rand_words;
+  if( !$opt_n ) {
+      @rand_words = $wl->get_words($count);
+  } else {
+    for( my $i; $i < $count; $i++ ) {
+      push @rand_words, "file" . $cnt++;
+    }
+    foreach(@rand_words) {
+      $_ =~ s/\'//g;
+    }
   }
-
   return @rand_words;
 }
 
@@ -91,6 +121,11 @@ sub create_dir_listing(@)
      }
   }
 }
+
+# main here.
+getopts('hn');
+
+help() if( $opt_h );
 
 srand();
 open_wordlist();
