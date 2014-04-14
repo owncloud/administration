161ea3fb1b3de8d62fa2cfc69a5c2ddca18fb270
Klaas Freitag <freitag@owncloud.com>
2014-04-14 11:35:48 +0200

    Reorg of code to be more flexible in reading substs.

--- jenkins/obs_integration/genbranding.pl
+++ jenkins/obs_integration/genbranding.pl
@@ -42,62 +42,78 @@ sub getFileName( $ ) {
   return $tarname;
 }
 
-sub createTarBall( $ ) {
-  my ($clientdir) = @_;
-  system("/bin/tar", "xif", $miralltar);
-  print "Extract mirall...\n";
-  my $mirall = getFileName( $ARGV[0] );
-  my $theme = getFileName( $ARGV[1] );
-  my $newname = $mirall;
-  $newname =~ s/-/-$theme-/;
-  move($mirall, $newname);
-  chdir($newname);
-  print "Extracting theme...\n";
-  system("/bin/tar", "--wildcards", "-xif", "$themetar", "*/mirall/*");
-  chdir("..");
-  print "Combining...\n";
-  system("/bin/tar", "cfi", "$clientdir/$newname.tar.bz2", "$newname");
-  rmtree("$newname");
+# Extracts the mirall tarball and puts the theme tarball the new dir
+sub prepareTarBall( ) {
+    print "Preparing tarball...";
+
+    system("/bin/tar", "xif", $miralltar);
+    print "Extract mirall...\n";
+    my $mirall = getFileName( $ARGV[0] );
+    my $theme = getFileName( $ARGV[1] );
+    my $newname = $mirall;
+    $newname =~ s/-/-$theme-/;
+    move($mirall, $newname);
+    chdir($newname);
+    print "Extracting theme...\n";
+    system("/bin/tar", "--wildcards", "-xif", "$themetar", "*/mirall/*");
+    chdir("..");
+
+    print " success: $newname\n";
+    return $newname;
+}
+
+# Create the final themed tarball 
+sub createTar($$)
+{
+    my ($clientdir, $newname) = @_;
+    print "Combining >$clientdir + $newname<\n";
+    my $tarName = "$clientdir/$newname.tar.bz2";
+    system("/bin/tar", "cjfi", $tarName, $newname);
+    rmtree("$newname");
+    print " success: Created $tarName\n";
+}
+
+# read all files from the template directory and replace the contents
+# of the .in files with values from the substition hash ref.
+sub createClientFromTemplate($) {
+    my ($substs) = @_;
+
+    print "Create client from template\n";
+    my $clienttemplatedir = "$templatedir/client";
+    my $theme = getFileName( $ARGV[1] );
+    mkdir("$theme-client");
+    chdir("$theme-client");
+    opendir(my $dh, $clienttemplatedir);
+    my $source;
+    # all files, excluding hidden ones, . and ..
+    my $tt = Template->new(ABSOLUTE=>1);
+ 
+    foreach my $source (grep ! /^\./,  readdir($dh)) {
+        my $target = $source;
+        $target =~ s/BRANDNAME/$theme/;
+        if($source =~ /\.in$/) {
+            $target =~ s/\.in$//;
+            $tt->process("$clienttemplatedir/$source", $substs, $target) or die $tt->error();
+        } else {
+            copy("$clienttemplatedir/$source", $target);
+        }
+     }
+     closedir($dh);
+     chdir("..");
+     return cwd();
 }
 
 
-sub createClientFromTemplate() {
-  my $clienttemplatedir = "$templatedir/client";
-  my $theme = getFileName( $ARGV[1] );
-  mkdir("$theme-client");
-  chdir("$theme-client");
-  opendir(my($dh), $clienttemplatedir);
-  my $source;
-  # all files, excluding hidden ones, . and ..
-  my $tt = Template->new(ABSOLUTE=>1);
-  my $substs =
-     {
-        shortname => "owncloud", #lowercase, e.g. as in owncloud-client
-        displayname => "ownCloud Client",
-        version => "1.5.3",
-        summary => "The ownCloud Client - Private file sync and share client",
-        projecturl => "https://github.com/owncloud/mirall",
-        tarball => "mirall-oem-1.5.3.tar.bz2",
-        pkgdescription => "Such summary\n\nMuch Text!\nVery product!",
-        pkgdescription_debian => "Such summary\n.\n Much Text!\n Very product!",
-        sysconfdir => "etc/ownCloud", #(etc/ownCloud, but lowercase for all OEMs...), without a leading slash
-        maintainer => "ownCloud Inc.",
-        maintainer_person => "Markus Rex <msrex@owncloud.com>",
-        desktopdescription => "ownCloud desktop sync client",
+sub getSubsts( $ ) 
+{
+    my ($subsDir) = @_;
 
-     };
-  foreach my $source (grep ! /^\./,  readdir($dh)) {
-    my $target = $source;
-    $target =~ s/BRANDNAME/$theme/;
-    if($source =~ /\.in$/) {
-      $target =~ s/\.in$//;
-      $tt->process("$clienttemplatedir/$source", $substs, $target) or die $tt->error();
-    } else {
-      copy("$clienttemplatedir/$source", $target);
-    }
-  }
-  closedir($dh);
-  return cwd();
+    my %substs;
+    # read the file package.cfg from the tarball and also remove it there evtl.
+    
+    # calculate some subst values, such as 
+    $substs{tarball} = $substs{shortname} . "-oem-" . $substs{version} . ".tar.bz2";
+    $substs{pkgdescription_debian} = $substs{pkgdescription};
 }
 
 # main here.
@@ -115,5 +131,11 @@ $templatedir = $dir .'/'. "templates";
 print "Mirall Tarball: $miralltar\n";
 print "Theme Tarball: $themetar\n";
 
-my $clientdir = createClientFromTemplate();
-createTarBall($clientdir);
+my $dirName = prepareTarBall();
+
+# returns hash reference
+my $substs = getSubsts($dirName);
+
+my $clientdir = createClientFromTemplate( $substs );
+
+createTar($clientdir, $dirName);
