40d17ca4904f1442d35acc321d293f780e49199f
Thomas Müller <thomas.mueller@tmit.eu>
2014-05-21 20:24:53 +0200

    initial set of travis scripts

---
+++ travis-ci/before_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# ownCloud
+#
+# @author Thomas Müller
+# @copyright 2014 Thomas Müller thomas.mueller@tmit.eu
+#
+
+WORKDIR=$PWD
+APP_NAME=$1
+CORE_BRANCH=$2
+echo "Work directory: $WORKDIR"
+cd ..
+git clone --depth 1 -b $CORE_BRANCH https://github.com/owncloud/core
+cd core
+git submodule update --init
+
+cd apps
+cp -R ../../$APP_NAME/ .
+cd $WORKDIR
+
+if [ "$1" == "mysql" ] ; then
+  mysql -e 'create database oc_autotest;'
+  mysql -u root -e "CREATE USER 'oc_autotest'@'localhost' IDENTIFIED BY 'owncloud'";
+  mysql -u root -e "grant all on oc_autotest.* to 'oc_autotest'@'localhost'";
+fi
+
+if [ "$1" == "pgsql" ] ; then
+  createuser -U travis -s oc_autotest
+fi
+
+if [ "$1" == "oracle" ] ; then
+  ./before_install_oracle.sh
+fi
