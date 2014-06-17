9e7e3d7f79dca8815d416d37b00b9e3372768f21
Jürgen Weigert <juewei@fabfolk.com>
2014-06-17 20:45:19 +0200

    script to populate all dependency packages into an oem:xxxx project.

---
+++ jenkins/obs_integration/setup_oem_client.pl
@@ -0,0 +1,45 @@
+#! /usr/bin/perl -w
+#
+# (c) 2014, jw@owncloud.com 
+#
+use Data::Dumper;
+sub list_obs_pkg;
+
+my @src_pkgs = qw{ neon libqt4 cmake libqt4 libqt4-sql-plugins qtwebkit qtkeychain };
+my $src_prj = 'desktop';
+my $dest_prj_prefix = 'oem:';
+my $obs_api = 'https://s2.owncloud.com';
+
+my $client_name = $ARGV[0];
+die "Usage: $0 oem:clientname\n" unless $client_name;
+$client_name =~ s{^oem:}{};
+
+my $dest_prj = $dest_prj_prefix . $client_name;
+my @existing_pkg = list_obs_pkg($obs_api, $dest_prj);
+my %existing_pkg = map { $_ => 1 } @existing_pkg;
+
+
+for my $pkg (@src_pkgs)
+  {
+    if ($existing_pkg{$pkg})
+      {
+        print STDERR "exists: $dest_prj $pkg\n";
+	next;
+      }
+    my $cmd = "osc -A$obs_api copypac $src_prj $pkg $dest_prj";
+    print STDERR "+ $cmd\n";
+    system($cmd);
+  }
+
+
+exit(0);
+
+sub list_obs_pkg()
+{
+  my ($api, $prj) = @_;
+  my $cmd = "osc -A$api ls $prj";
+  open(my $ifd, "$cmd 2>/dev/null|") or die "list_obs_pkg: cannot read from '$cmd'";
+  my @pkgs = <$ifd>;
+  chomp @pkgs;
+  return @pkgs;
+}
