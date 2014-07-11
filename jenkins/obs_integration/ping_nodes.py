56680a760073e3c812b811c62ce9bea2c4dea649
Jürgen Weigert <jw@owncloud.com>
2014-07-11 14:28:51 +0200

    made xpassword optional, allow also plaintext password.

--- jenkins/obs_integration/ping_nodes.py
+++ jenkins/obs_integration/ping_nodes.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+#
+# (c) 2014 jw@owncloud.com
+# GPLv2.0 or ask.
+################################
+# https://python-jenkins.readthedocs.org/en/latest/
 
 import jenkinsapi
 from jenkinsapi.jenkins import Requester
@@ -9,7 +14,9 @@ import base64,os,ConfigParser
 cp = ConfigParser.ConfigParser()
 cp.read(os.path.expanduser('~/.jenkins'))
 cfg=dict(cp.items('api'))
-cfg['password']=base64.decodestring(cfg['xpassword'])
+if not cfg.has_key('password'): cfg['password']=base64.decodestring(cfg['xpassword'])
+# API docs say this should work, too. It does not.
+# cfg['password']=cfg['token']
 
 j = Jenkins(cfg['baseurl'], username=cfg['username'], password=cfg['password'], requester=Requester(cfg['username'],cfg['password'], baseurl=cfg['baseurl'], ssl_verify=False))
 
