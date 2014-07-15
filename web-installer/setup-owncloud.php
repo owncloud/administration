6f97037e9a23116c921c2f459ae45949d79bf36a
born2webdesign <social@born2webdesign.de>
2014-07-15 23:39:10 +0200

    Avoid downloading oc.zip if it already exists
    If we already have oc.zip lying around from a previous download, there
    is no need for the web-installer to download it again. E.g. while
    hacking on the installer, I keep a local copy of the file around.

--- web-installer/setup-owncloud.php
+++ web-installer/setup-owncloud.php
@@ -93,7 +93,9 @@ class oc_setup {
 		$error='';
 		
 		// downloading latest release
-		$error.=oc_setup::getfile('https://download.owncloud.org/download/community/owncloud-latest.zip','oc.zip');
+		if (!file_exists('oc.zip')) {
+			$error.=oc_setup::getfile('https://download.owncloud.org/download/community/owncloud-latest.zip','oc.zip');
+		}
 		
 		// unpacking into owncloud folder
 		$zip = new ZipArchive;
