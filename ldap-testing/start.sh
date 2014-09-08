3ec97609cef0908f90050a3207b623c4e6237462
Vincent Petry <pvince81@owncloud.com>
2014-09-08 22:22:29 +0200

    Improve docker script for LDAP
    Move config env to top.
    Use better method to get container IP

--- ldap-testing/start.sh
+++ ldap-testing/start.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+# config
+PHPLDAPADMIN_LOCAL_PORT=8443
+LDAP_LOCAL_PORT=389
+
+LDAP_DOMAIN=owncloud.com
+LDAP_ORGANISATION=ownCloud
+LDAP_ROOTPASS=admin
+
+LDAP_BASE_DN=dc=owncloud,dc=com
+LDAP_LOGIN_DN=cn=admin,dc=owncloud,dc=com
+
 # start containers
-docker run -p 127.0.0.1:389:389 \
-	-e LDAP_DOMAIN=owncloud.com \
-	-e LDAP_ORGANISATION="ownCloud" \
-	-e LDAP_ROOTPASS=admin \
+docker run -p 127.0.0.1:$LDAP_LOCAL_PORT:389 \
+	-e LDAP_DOMAIN=$LDAP_DOMAIN \
+	-e LDAP_ORGANISATION="$LDAP_ORGANISATION" \
+	-e LDAP_ROOTPASS=$LDAP_ROOTPASS \
 	--name docker-slapd \
 	-d nickstenning/slapd || exit 1
 
 #docker inspect docker-slapd | grep IP
-SLAPD_CONTAINER_IP=$(docker inspect docker-slapd | grep IPAddress | sed -e 's/.*:\s"\([0-9.]*\)".*/\1/g')
-echo "LDAP server now available under 127.0.0.1:389 (internal IP is $SLAPD_CONTAINER_IP)"
-docker run -p 443:443 \
+SLAPD_CONTAINER_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" docker-slapd)
+docker run -p $PHPLDAPADMIN_LOCAL_PORT:443 \
 	--name docker-phpldapadmin \
 	-e LDAP_HOST=$SLAPD_CONTAINER_IP \
-	-e LDAP_BASE_DN=dc=owncloud,dc=com \
-	-e LDAP_LOGIN_DN=cn=admin,dc=owncloud,dc=com \
+	-e LDAP_BASE_DN=$LDAP_BASE_DN \
+	-e LDAP_LOGIN_DN=$LDAP_LOGIN_DN \
 	-d osixia/phpldapadmin || exit 2
 
-echo "phpldapadmin now available under https://127.0.0.1"
-
 docker ps
+echo
+echo "LDAP server now available under 127.0.0.1:$LDAP_LOCAL_PORT (internal IP is $SLAPD_CONTAINER_IP)"
+echo "phpldapadmin now available under https://127.0.0.1:$PHPLDAPADMIN_LOCAL_PORT"
+echo
 
