1950fdf83157870dd2a0b37e80c53ec58aec0c60
Jürgen Weigert <jw@owncloud.com>
2015-07-16 15:45:34 +0200

    setup_build_targets now pulls repo config from TEMPLATE_PRJ desktop. Fixed https://github.com/owncloud/ownbrander/issues/393

--- jenkins/obs_integration/setup_build_targets.pl
+++ jenkins/obs_integration/setup_build_targets.pl
@@ -9,16 +9,21 @@
 # v0.2, 2015-03-08, jw, option parser, done, obs meta pkg parser done, meta pkg setter todo.
 # v0.3, 2015-03-09, jw, meta pkg setter done. wipebinaries option added.
 # v0.4, 2015-03-31, jw, allow _x86 suffix in set32.
+# v0.5, 2015-07-15, jw, always sync list of available repositories from TEMPLATE_PRJ.
+#                       Fixing https://github.com/owncloud/ownbrander/issues/393
 
 use Data::Dumper;
 use XML::Simple;
 
-my $version = '0.4';
+my $version = '0.5';
 my $verbose = 1;
 my $obs_api = $ENV{'OBS_API'} || 'https://s2.owncloud.com';
 my $osc_cmd = $ENV{'OSC_CMD'} || 'osc';
+my $template_prj = $ENV{'TEMPLATE_PRJ'} || 'desktop';
 
 my $obs_proj = $ARGV[0];
+$obs_proj = '' if $obs_proj =~ m{^-};	# someone tried --help, or so?
+
 die qq{
 Usage: $0 OBS_PROJ [set32='list,of,target,names' set64='list,of,target,names']
        $0 OBS_PROJ [set='list,of,target,names']
@@ -35,12 +40,20 @@ The project must have the needed targets pre-configured in its meta prj xml.
 Wildcards (shell globbing style) are supported. E.g. 'set=*' enables all 
 packages to build for all targets that are currently configured in the project.
 
-Targets listed with set* commands must be configured repositories in OBS_PROJ.
-Lower case spelling is accepted. Characters _, :, - are considered equal.
+Targets listed with set* commands must be configured repositories in the template project
+'$template_prj'.
+Lower case spelling for targets is accepted. Characters _, :, - are considered equal.
+
+Environment variables:
+ OSC_CMD	 current default: $osc_cmd
+ OBS_API	 current default: $obs_api
+ TEMPLATE_PRJ	 current default: $template_prj
+ NO_WIPEBINARIES current default: 0
 
 } unless $obs_proj;
 
 my @existing_pkg = list_obs_pkg($obs_proj);
+update_obs_repos($obs_proj) if $ARGV[1] and length($template_prj);	 # we are going to set something...
 my $configured_repos = list_obs_repos($obs_proj);
 
 my $repo_aliases = mk_repo_aliases(keys %{$configured_repos->{32}},
@@ -50,20 +63,32 @@ my $repo_aliases = mk_repo_aliases(keys %{$configured_repos->{32}},
 
 unless ($ARGV[1])
   {
+    my $last_setting = '';
+    my $setting = '';
     for my $pkg (@existing_pkg)
       {
         my $targets = list_enabled_targets($obs_proj, $pkg);
         if ($targets->{'ena'})
           {
-	    printf "%20s: enabled=%s\n", $pkg, join(',', @{$targets->{ena}});
+	    $setting="enabled=".join(',', @{$targets->{ena}});
           }
         elsif ($targets->{'dis'})
           {
-	    printf "%20s: disabled=%s\n", $pkg, join(',', @{$targets->{dis}});
+	    $setting="disabled=".join(',', @{$targets->{dis}});
           }
 	else
 	  {
-	    printf "%20s: default\n", $pkg;
+	    $setting="default";
+	  }
+
+	if ($setting eq $last_setting)
+	  {
+	    printf "%20s: ... same\n", $pkg;
+	  }
+	else
+	  {
+	    printf "%20s: %s\n", $pkg, $setting;
+	    $last_setting = $setting;
 	  }
       }
     exit 0;
@@ -222,6 +247,52 @@ sub list_enabled_targets
       push @{$l->{'dis'}}, $t if $t;
     }
   return $l;
+}
+
+sub update_obs_repos
+{
+  #<project>
+  #   ...
+  #   <repository name="CentOS_6">
+  #    <path project="openSUSE.org:CentOS:CentOS-6" repository="standard"/>
+  #    <arch>i586</arch>
+  #    <arch>x86_64</arch>
+  #  </repository>
+  #</project>
+  my ($prj) = @_;
+
+  my $cmd = "$osc_cmd -A$obs_api meta prj $prj";
+  print STDERR "+ $cmd\n" if $verbose > 1;
+  open(my $ifd, "$cmd 2>/dev/null|") or die "configured_repos: cannot read from '$cmd'";
+  my $meta_prj_xml = join('', <$ifd>);
+  close($ifd);
+
+  $cmd = "$osc_cmd -A$obs_api meta prj $template_prj";
+  print STDERR "+ $cmd\n" if $verbose > 1;
+  open($ifd, "$cmd 2>/dev/null|") or die "configured_repos: cannot read from '$cmd'";
+  my $meta_prj_xml_t = join('', <$ifd>);
+  close($ifd);
+
+  # remove all that is not repository definition
+  $meta_prj_xml_t = $1 if $meta_prj_xml_t =~ m{(<repository\b.*</repository>)}s;
+  $have_repos     = $1 if $meta_prj_xml   =~ m{(<repository\b.*</repository>)}s;
+
+  if ($meta_prj_xml_t eq $have_repos)
+     {
+       print "Good: $obs_proj and $template_prj have same repos.\n";
+       return;
+     }
+  print "Template project '$template_prj' differs, pulling changes...\n";
+
+  # merge into $meta_prj_xml
+  $meta_prj_xml =~ s{<repository\b.*</repository>}{<!-- merged from $template_prj -->$meta_prj_xml_t<!-- end merge -->}s;
+
+  # write back the new build structure
+  $cmd = "$osc_cmd -A$obs_api meta prj -F - $prj";
+  print STDERR "+ $cmd\n" if $verbose > 2;
+  open($ifd, "|$cmd") or die "update_obs_repos: cannot write to '$cmd'";
+  print $ifd $meta_prj_xml;
+  close($ifd) or die "update_obs_repos: could not write to '$cmd'";
 }
 
 sub list_obs_repos
