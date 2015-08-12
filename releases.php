42edba7716e9d65db15d87577583b2a27a5a3ae4
Morris Jobke <hey@morrisjobke.de>
2015-08-12 09:29:33 +0200

    update due dates of milestone, add/rename labels

--- releases.php
+++ releases.php
@@ -5,6 +5,7 @@ require_once 'vendor/autoload.php';
 $COLOR_GRAY = "\033[0;37m";
 $COLOR_RED = "\033[0;31m";
 $NO_COLOR = "\033[0m";
+$STRIKE_THROUGH = "\033[9m";
 
 $client = new \Github\Client(
 	new \Github\HttpClient\CachedHttpClient([
@@ -32,6 +33,12 @@ $paginator  = new Github\ResultPager($client);
 
 $config = json_decode(file_get_contents('config.json'));
 $repositories = [];
+
+$updateDueDate = [];
+
+$SHOW_MILESTONE = true;
+$SHOW_LABEL = true;
+
 foreach($config->repos as $repo) {
 	$repositories[$repo] = [
 		'milestones' => [],
@@ -39,42 +46,68 @@ foreach($config->repos as $repo) {
 	];
 
 	print('Repo ' . $config->org . '/' . $repo . PHP_EOL);
-	print("  Milestones" . PHP_EOL);
+	if($SHOW_MILESTONE) print("  Milestones" . PHP_EOL);
 	$milestones = $client->api('issue')->milestones()->all($config->org, $repo);
 	uasort($milestones, 'milestoneSort');
 	foreach($milestones as $milestone) {
 		$repositories[$repo]['milestones'][$milestone['title']] = $milestone['open_issues'];
 
-		print("    ");
+		if($SHOW_MILESTONE) print("    ");
 		if($milestone['open_issues'] !== 0) {
-			print($milestone['title'] . ' ' . $milestone['open_issues']);
+			if($SHOW_MILESTONE) print($milestone['title'] . ' ' . $milestone['open_issues']);
 		} else {
-			print($COLOR_GRAY. $milestone['title']);
+			if($SHOW_MILESTONE) print($COLOR_GRAY. $milestone['title']);
 		}
-		print($NO_COLOR . PHP_EOL);
+		if(property_exists($config->dueDates, $milestone['title']) &&
+			$milestone['due_on'] !== $config->dueDates->$milestone['title'] . 'T04:00:00Z') {
+			if($SHOW_MILESTONE) print($COLOR_RED . ' update due date');
+			$updateDueDate[] = [
+				'org' => $config->org,
+				'repo' => $repo,
+				'number' => $milestone['number'],
+				'milestone' => $milestone['title'],
+				'state' => $milestone['state'],
+				'title' => $milestone['title'],
+				'description' => $milestone['description'],
+				'oldDueDate' => $milestone['due_on'],
+				'newDueDate' => $config->dueDates->$milestone['title'] . 'T04:00:00Z',
+			];
+		}
+		if($SHOW_MILESTONE) print($NO_COLOR . PHP_EOL);
+
 	}
 
 	if(in_array($repo, $config->skipLabels)) {
 		continue;
 	}
-	print("  Labels" . PHP_EOL);
+	if($SHOW_LABEL) print("  Labels" . PHP_EOL);
 	$labels = $paginator->fetchAll($client->api('issues')->labels(), 'all', [$config->org, $repo]);
 	uasort($labels, 'labelSort');
 	foreach($labels as $label) {
 		if($label['name'][1] === '.') {
 			$repositories[$repo]['labels'][$label['name']] = null;
 
-			if(strpos($label['name'], '-current') === false && strpos($label['name'], '-next') === false ) {
-				print($COLOR_GRAY);
+			if(strpos($label['name'], '-current') === false) {
+				if($SHOW_LABEL) print($COLOR_GRAY);
 			}
-			print("    " . $label['name']);
+			if($SHOW_LABEL) print("    " . $label['name']);
+			$repositories[$repo]['labels'][$label['name']] = [
+				'color' => $label['color']
+			];
 			if(strpos($label['name'], '-current') !== false) {
 				$issues = $client->api('issue')->all($config->org, $repo, ['labels' => $label['name']]);
-				$count = count($issues);
-				print(' ' . $count);
-				$repositories[$repo]['labels'][$label['name']] = $count;
+				$openCount = count($issues);
+				if($SHOW_LABEL) print(' ' . $openCount);
+				$issues = $client->api('issue')->all($config->org, $repo, ['labels' => $label['name'], 'state' => 'closed']);
+				$closedCount = count($issues);
+				if($SHOW_LABEL) print(' ' . $closedCount);
+				$repositories[$repo]['labels'][$label['name']] = [
+					'color' => $label['color'],
+					'open' => $openCount,
+					'closed' => $closedCount,
+				];
 			}
-			print($NO_COLOR . PHP_EOL);
+			if($SHOW_LABEL) print($NO_COLOR . PHP_EOL);
 		}
 	}
 }
@@ -94,5 +127,40 @@ foreach($repositories as $name => $repository) {
 	}
 	if(!array_key_exists('8.0.5', $repository['labels'])) {
 		print($COLOR_RED . '8.0.5 missing in ' . $config->org . '/' . $name . $NO_COLOR . PHP_EOL);
+	}
+
+	foreach($repository['labels'] as $label => $info) {
+		if(array_key_exists($label, $config->renameLabels)) {
+			print($COLOR_RED . $config->org . '/' . $name . ': rename label ' . $label . ' -> ' . $config->renameLabels->$label . $NO_COLOR . PHP_EOL);
+			continue;
+			// TODO ask for the update
+			$client->api('issue')->labels()->update($config->org, $name, $label, $config->renameLabels->$label, $info['color']);
+		}
+	}
+
+	foreach($config->addLabels as $label) {
+		if(!array_key_exists($label, $repository['labels'])) {
+			print($COLOR_RED . $config->org . '/' . $name . ': add label ' . $label . $NO_COLOR . PHP_EOL);
+			#continue;
+			// TODO ask for the update
+			$client->api('issue')->labels()->create($config->org, $name, [ 'name' => $label, 'color' => '996633' ]);
+
+		}
+	}
+}
+
+if(count($updateDueDate)) {
+	print('Following due dates need to be updated:' . PHP_EOL);
+
+	foreach($updateDueDate as $date) {
+		print($COLOR_RED . $date['org'] . '/' . $date['repo'] . ' ' . $date['title'] . ' from ' . $date['oldDueDate'] . ' to ' . $date['newDueDate'] . $NO_COLOR . PHP_EOL);
+		#continue;
+		// TODO ask for the update
+		$client->api('issue')->milestones()->update($date['org'], $date['repo'], $date['number'], [
+			'title' => $date['title'],
+			'state' => $date['state'],
+			'description' => $date['description'],
+			'due_on' => $date['newDueDate']
+		]);
 	}
 }
