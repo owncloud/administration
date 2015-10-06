373ee03a96eb5044226bf1fdd838d874791fd462
Jürgen Weigert <jnweiger@gmail.com>
2015-10-07 00:36:27 +0200

    using --commit twice for non-interactove

--- jenkins/obs_integration/server_tar_to_obs.sh
+++ jenkins/obs_integration/server_tar_to_obs.sh
@@ -26,7 +26,7 @@ if [ -z "$VERSIONS" ]; then
 fi
 
 # cmd="../../obs-new-tar.py -e $username "
-cmd="../../obs-new-tar.py --commit -e $username "
+cmd="../../obs-new-tar.py --commit --commit -e $username "	# Hack: using --commit twice is non-interactive.
 submitreq=0	# switch to 1, to also create submitrequests from $prj$prjsuffix to $prj
 test -z "$prerel"  && submitreq=1
 
