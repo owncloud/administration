e512f990feeba0179bf3d8ba5389d3e4d624742c
Morris Jobke <hey@morrisjobke.de>
2015-10-14 14:24:07 +0200

    Pull docker images to have them up to date
    - fix another env variable to match the new images

--- ldap-testing/start.sh
+++ ldap-testing/start.sh
@@ -11,6 +11,8 @@ LDAP_ROOTPASS=admin
 LDAP_BASE_DN=dc=owncloud,dc=com
 LDAP_LOGIN_DN=cn=admin,dc=owncloud,dc=com
 
+docker pull nickstenning/slapd
+
 # start containers
 docker run -p 127.0.0.1:$LDAP_LOCAL_PORT:389 \
 	-e LDAP_DOMAIN=$LDAP_DOMAIN \
@@ -19,11 +21,13 @@ docker run -p 127.0.0.1:$LDAP_LOCAL_PORT:389 \
 	--name docker-slapd \
 	-d nickstenning/slapd || exit 1
 
+docker pull osixia/phpldapadmin
+
 #docker inspect docker-slapd | grep IP
 SLAPD_CONTAINER_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" docker-slapd)
 docker run -p $PHPLDAPADMIN_LOCAL_PORT:443 \
 	--name docker-phpldapadmin \
-	-e LDAP_HOSTS=$SLAPD_CONTAINER_IP \
+	-e PHPLDAPADMIN_LDAP_HOSTS=$SLAPD_CONTAINER_IP \
 	-e LDAP_BASE_DN=$LDAP_BASE_DN \
 	-e LDAP_LOGIN_DN=$LDAP_LOGIN_DN \
 	-d osixia/phpldapadmin || exit 2
