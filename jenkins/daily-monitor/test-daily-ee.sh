62d0526f757ddb8e31a92cbba8e3df3cb90926c5
Thomas Müller <thomas.mueller@tmit.eu>
2016-02-22 20:09:15 +0100

    EE version of the daily tar tester

---
+++ jenkins/daily-monitor/test-daily-ee.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: test-daily <from-version>"
+    echo "Example: test-daily master"
+    exit
+fi
+
+DATE=`date +%Y-%m-%d`
+FROM_VERSION=$1
+FROM=owncloud-daily-enterprise-complete-$FROM_VERSION.tar.bz2
+
+rm -rf $FROM
+rm -rf owncloud
+
+wget http://download.owncloud.com/internal/$FROM
+
+tar -xjf $FROM
+
+DATE_IN_VERSION=`grep OC_Build owncloud/version.php | cut -d "'" -f 2 | cut -d "T" -f1`
+
+if [ "$DATE" != "$DATE_IN_VERSION" ]; then
+    echo "Daily tar $FROM has not been updated - build date is $DATE_IN_VERSION!"
+    exit 1
+fi
+echo "Daily tar $FROM is up to date"
