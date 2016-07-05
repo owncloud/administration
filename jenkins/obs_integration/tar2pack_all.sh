e5dc30cdceacd5c3ab9565f8b240356b81b4c503
Juergen Weigert <jw@owncloud.com>
2016-07-05 18:52:44 +0200

    finally we get closer to a world dominating build service script.

--- jenkins/obs_integration/tar2pack_all.sh
+++ jenkins/obs_integration/tar2pack_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-# yet another script to do packaging.
+# Yet another script to do packaging.
+# lessons lerned: 
+#  - unroll everything for easier maintenance.
+#  - there is always some manual changes in the templates. better run with echo=echo and do copy-pasta
+#  - always push into :testing, and if its a final then add a pull request
 #
 # 2016-07-04, jw@owncloud.com
 
@@ -35,48 +39,45 @@ for vers in $*; do
   esac
 
   case $vers in
-  9.1*) majmin=9.1
-  ;;
+  # 9.1*) majmin=9.1 ;;
 
-  9.0*) majmin=9.0
-  ;;
+  # 9.0*) majmin=9.0 ;;
 
-  8.2*) majmin=8.2
-  ;;
+  # 8.2*) majmin=8.2 ;;
 
   8.1*) majmin=8.1
 
-  $echo cd $co_dir_s2/ee:$majmin:testing/owncloud-enterprise
-  $echo osc up
-  $echo $tar2pack -O . http://$user:$pass@download.owncloud.com/internal/$vers/owncloud-enterprise-complete-$vers.tar.bz2 -d SOURCE_TAR_TOP_DIR=owncloud
-  $echo wget https://doc.owncloud.org/server/8.1/ownCloud_Server_Administration_Manual.pdf
-  $echo osc addremove
-  $echo osc ci
-  test -z "$testing" && $echo osc submitpac --no-cleanup ee:$majmin owncloud-enterprise
+    $echo cd $co_dir_s2/ee:$majmin:testing/owncloud-enterprise
+    $echo osc up
+    $echo $tar2pack -O . http://$user:$pass@download.owncloud.com/internal/$vers/owncloud-enterprise-complete-$vers.tar.bz2 -d SOURCE_TAR_TOP_DIR=owncloud
+    $echo wget https://doc.owncloud.org/server/8.1/ownCloud_Server_Administration_Manual.pdf
+    $echo osc addremove
+    $echo osc ci
+    test -z "$testing" && $echo osc submitpac --no-cleanup ee:$majmin owncloud-enterprise
 
-  $echo cd $co_dir_obs/isv:ownCloud:community:$majmin:testing/owncloud
-  $echo osc up
-  $echo $tar2pack -O . http://download.owncloud.org/community/$testing/owncloud-$vers.tar.bz2
-  $echo osc addremove
-  $echo osc ci
-  test -z "$testing" && $echo osc submitpac --no-cleanup isv:ownCloud:community:$majmin owncloud-enterprise
+    $echo cd $co_dir_obs/isv:ownCloud:community:$majmin:testing/owncloud
+    $echo osc up
+    $echo $tar2pack -O . http://download.owncloud.org/community/$testing/owncloud-$vers.tar.bz2
+    $echo osc addremove
+    $echo osc ci
+    test -z "$testing" && $echo osc submitpac --no-cleanup isv:ownCloud:community:$majmin owncloud-enterprise
   ;;
 
   8.0*) majmin=8.0
 
-  $echo cd $co_dir_s2/ee:$majmin:testing/owncloud-enterprise
-  $echo osc up
-  $echo $tar2pack -O . http://$user:$pass@download.owncloud.com/internal/$vers/owncloud-enterprise-$vers.tar.bz2 -d SOURCE_TAR_TOP_DIR=owncloud
-  $echo osc addremove
-  $echo osc ci
-  test -z "$testing" && $echo osc submitpac --no-cleanup ee:$majmin owncloud-enterprise
+    $echo cd $co_dir_s2/ee:$majmin:testing/owncloud-enterprise
+    $echo osc up
+    $echo $tar2pack -O . http://$user:$pass@download.owncloud.com/internal/$vers/owncloud-enterprise-$vers.tar.bz2 -d SOURCE_TAR_TOP_DIR=owncloud
+    $echo osc addremove
+    $echo osc ci
+    test -z "$testing" && $echo osc submitpac --no-cleanup ee:$majmin owncloud-enterprise
 
-  $echo cd $co_dir_obs/isv:ownCloud:community:$majmin:testing/owncloud
-  $echo osc up
-  $echo $tar2pack -O . http://download.owncloud.org/community/$testing/owncloud-$vers.tar.bz2
-  $echo osc addremove
-  $echo osc ci
-  test -z "$testing" && $echo osc submitpac --no-cleanup isv:ownCloud:community:$majmin owncloud-enterprise
+    $echo cd $co_dir_obs/isv:ownCloud:community:$majmin:testing/owncloud
+    $echo osc up
+    $echo $tar2pack -O . http://download.owncloud.org/community/$testing/owncloud-$vers.tar.bz2
+    $echo osc addremove
+    $echo osc ci
+    test -z "$testing" && $echo osc submitpac --no-cleanup isv:ownCloud:community:$majmin owncloud-enterprise
   ;;
 
   esac
@@ -88,3 +89,13 @@ for vers in $*; do
 
 done
 
+if [ -z "$testing" ]; then
+  echo "new pull requests:"
+  osc rq list isv:ownCloud:community -s new,review
+  osc -As2 rq list -s new,review
+  echo "Please accept the pull request to build non-testing packages."
+  echo "Caution: at openSUSE obs, you may want to disable publishing first.
+fi
+
+echo "ssh root@s2"
+echo "# and do bin/publishee ...; bin/publish_ce ... eventually."
