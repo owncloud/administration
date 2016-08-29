e94345bac9df0ee03d65d89d53a5551acaaeb130
Juergen Weigert <jw@owncloud.com>
2016-08-29 21:31:46 +0200

    - added md5sum comparison to avoid repeated copies. - added echo commands to squash newlines.

--- jenkins/obs_integration/obs-migration/obs-copy-prj.sh
+++ jenkins/obs_integration/obs-migration/obs-copy-prj.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#! /bin/sh
 #
 # see http://openbuildservice.org/help/manuals/obs-best-practices/cha.obs.best-practices.bootstrapping.html
 #
+# We only copy when md5sum changes!
 
 test -z "$OBS_API_DST"  && OBS_API_DST=obs-new
 test -z "$OBS_API_SRC"  && OBS_API_SRC=obs-old
@@ -19,26 +20,33 @@ if [ -z "$1" ]; then
   exit 0
 fi
 
-echo press ENTER to continue
+echo "press ENTER to continue"
 read a
 
-set -x
 for prj in "$@"; do
+  echo "copying project $prj ..."
   if [ -z "$(osc -A$OBS_API_DST meta prj $prj)" ]; then
     echo "creating $prj at $OBS_API_DST ..."
     # remove all users, add myself as maintainer
-    osc -A$OBS_API_SRC meta prj $prj | grep -v userid= | sed -e 's@</project>@<person userid="'$my_id'" role="maintainer"/></project>@' | osc -A$OBS_API_DST meta prj $prj -F -
+    osc -A$OBS_API_SRC meta prj $prj | grep -v userid= | sed -e 's@</project>@<person userid="'$OBS_USER_DST'" role="maintainer"/></project>@' | osc -A$OBS_API_DST meta prj $prj -F - --force || exit 1
     # also copy prjconf
-    osc -A$OBS_API_SRC meta prjconf $prj | osc -A$OBS_API_DST meta prjconf $prj -F -
+    osc -A$OBS_API_SRC meta prjconf $prj | osc -A$OBS_API_DST meta prjconf $prj -F - --force || exit 1
   fi
-  echo "copying project $prj ..."
   pkgs=$(osc -A$OBS_API_SRC ls $prj)
   npkgs=$(echo $pkgs | wc -w)
   counter=0
+  echo "Packages in $prj: $(echo $pkgs)"
   for pkg in $pkgs; do
     counter=$(expr $counter + 1)
-    echo "$prj: $counter/npkgs ..."
-    osc -A$OBS_API_SRC copypac -t $OBS_API_DST $prj $pkg $prj || exit 1
+    echo "$prj: $counter/$npkgs ..."
+    md5_old=$(osc -A$OBS_API_SRC log $prj $pkg 2>&1 | head -n2 | awk -F\| '{ print $4 }')
+    md5_new=$(osc -A$OBS_API_DST log $prj $pkg 2>&1 | head -n2 | awk -F\| '{ print $4 }')
+    if [ "$md5_old" = "$md5_new" ]; then
+      echo "$prj $pkg md5sum $(echo $md5_new) matches. Skipping ..."
+    else
+      echo "+ osc -A$OBS_API_SRC copypac -t $OBS_API_DST $prj $pkg $prj"
+      osc -A$OBS_API_SRC copypac -t $OBS_API_DST $prj $pkg $prj || exit 1
+    fi
   done
 done
 
