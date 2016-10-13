acba77e310f6a9717f6a98662b7b6b61ca4662c9
Juergen Weigert <jw@owncloud.com>
2016-10-13 19:43:46 +0200

    build environment for CentOS_7_SCL_PHP55

--- docker/build-php5-libsmbclient/RUN.sh
+++ docker/build-php5-libsmbclient/RUN.sh
@@ -1,1 +1,9 @@
-kj
+#!/bin/bash
+name=centos7-php55-devel
+docker build -t $name -f Dockerfile.centos7 .
+cat <<EOF
+Study:
+  https://github.com/owncloud/documentation/issues/2172#issuecomment-188876694
+Try:
+  docker run -ti $name
+EOF
