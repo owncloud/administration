be872bedd548c259e38e464418e850d71b9a1296
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-11-07 23:36:25 +0100

    Update before_install.sh

--- travis-ci/before_install.sh
+++ travis-ci/before_install.sh
@@ -28,10 +28,10 @@ cd $WORKDIR
 
 if [ "$DB" == "mysql" ] ; then
   echo "Setting up mysql ..."
-  mysql -e 'create database oc_autotest;'
+  mysql -u root -e 'create database oc_autotest;'
   mysql -u root -e "CREATE USER 'oc_autotest'@'localhost' IDENTIFIED BY 'owncloud'";
   mysql -u root -e "grant all on oc_autotest.* to 'oc_autotest'@'localhost'";
-  mysql -e "SELECT User FROM mysql.user;"
+  mysql -u root -e "SELECT User FROM mysql.user;"
 fi
 
 if [ "$DB" == "pgsql" ] ; then
