e69cd2c1d475ffad1c213c8f93e5bcd45a9d5b92
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-03-06 21:01:30 +0300

    Allow 8.2.10 to 9.0.8 for stable channel

--- update-server/config/config.php
+++ update-server/config/config.php
@@ -163,6 +163,10 @@ return [
 			'latest' => '8.2.10',
 			'web' => 'https://doc.owncloud.org/server/8.2/admin_manual/maintenance/upgrade.html',
 		],
+		'8.2.10' => [
+			'latest' => '9.0.8',
+			'web' => 'https://doc.owncloud.org/server/9.0/admin_manual/maintenance/upgrade.html',
+		],
 		'8.1' => [
 			'latest' => '8.1.12',
 			'web' => 'https://doc.owncloud.org/server/8.1/admin_manual/maintenance/upgrade.html',
