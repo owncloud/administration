28bc80673ee9041f28234c6c6070f481fff9e4de
Juergen Weigert <jw@owncloud.com>
2017-04-12 11:16:10 +0200

    prepare_tarball.sh almost done.

--- jenkins/obs_integration/genbranding/prepare_tarball.sh
+++ jenkins/obs_integration/genbranding/prepare_tarball.sh
@@ -9,27 +9,28 @@
 clienttar=$1
 themetar=$2
 outtar=$3
-client=$(basename $clienttar | sed -e 's@.tar.*$@@)
-theme=$(basename $themetar | sed -e 's@.tar.*$@@)
-if [ "$theme" != 'ownCloud' ]; then
+client=$(basename $clienttar | sed -e 's|.tar.*$||')
+theme=$(basename $themetar | sed -e 's|.tar.*$||')
+if [ "$theme" = 'ownCloud' ]; then
 	echo "Creating the original ownCloud package tarball!"
 	newname=$(echo $client | tr '[:upper:]' '[:lower:]')
 else
 	# note: no -client suffix. works with setup_all_oem_clients.pl
 	# note that we add a - here.
-	newname=$(echo $theme | sed -e "s/^client/$theme/i" -e "s/^owncloud/$theme/i")
+	newname=$(echo $client | sed -e "s/^client/$theme/i" -e "s/^owncloud/$theme/i")
 fi
 
 tmpdir=tmp$$
 rm -rf $tmpdir; mkdir -p $tmpdir
 /bin/tar xif $clienttar --force-local -C $tmpdir
-if [ "$newname" != "$client" ];
+if [ "$newname" != "$client" ]; then
 	mv $tmpdir/$client $tmpdir/$newname
 fi
+echo newname is $newname
 tarwild="/bin/tar --wildcards --force-local -xif $themetar -C $tmpdir/$newname"
 $tarwild '*/mirall/*'
 $tarwild '*/syncclient/*'
-ls -la $tmpdir/$newname/*
-rm -rf $tmpdir
+# du -a $tmpdir/$newname
+# rm -rf $tmpdir
 
 # compile with cmake -DOEM_THEME_DIR=$PWD/../testpilotcloud/syncclient ...
