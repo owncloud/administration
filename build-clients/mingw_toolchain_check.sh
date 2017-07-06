9f53d2d1a37695e3922e5b9a1040a0af268f7967
Juergen Weigert <jw@owncloud.com>
2017-07-06 15:44:14 +0200

    script to check the mingw toolchain for remote links.

---
+++ build-clients/mingw_toolchain_check.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# This script checks the links in the project.
+# It ignores links that remain in the same project.
+# It reports all links pointing outside and gives recommendations how to resolve them. 
+
+BRANCH=2.3
+prj=isv:ownCloud:toolchains:mingw:win32:$BRANCH
+test -z "$1" || prj=$1
+test -z "$OBS_API" && OBS_API=https://api.opensuse.org
+
+echo "... checking $OBS_API project $prj"
+
+pkgs=$(osc -A$OBS_API ls $prj)
+
+for pkg in $pkgs; do
+  echo "\t$pkg ..."
+  haslink=
+  lin=$(osc -Aobs cat -u $prj $pkg _link 2>/dev/null) && haslink=true
+  if [ "$haslink" = "true" ]; then
+    if (echo "$lin" | grep -q '\bproject="'); then
+      if (echo $lin | grep '\bproject="' | grep -q "project=\"isv"); then
+        echo "$pkg: harmless local project link."
+      else
+        echo "ERROR: $pkg has link with remote project found: $lin"
+      fi
+    else
+        echo "$pkg: harmless link with no project."
+    fi
+  fi
+done
