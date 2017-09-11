8ff85de128c9ac899b4c83081ac6e936f24085fc
Juergen Weigert <jw@owncloud.com>
2017-09-11 15:14:50 +0200

    all animals are dangeroos. (OSS, NON-OSS, OSS Update all disabled now)

--- jenkins/win32_build/compile_client_as_user_with_nsis3.sh
+++ jenkins/win32_build/compile_client_as_user_with_nsis3.sh
@@ -8,14 +8,17 @@ zypper mr -d http://download.opensuse.org/repositories/windows:/mingw/openSUSE_4
 # 2017-09-10, jw: the main 42.1 repos are removed from OBS. Only 'OSS Update' remains.
 zypper mr -d OSS
 zypper mr -d NON-OSS
+zypper mr -d 'OSS Update'
 
-zypper --non-interactive --gpg-auto-import-keys install sudo
+# Not needed. sudo is already onboard in alfageme/docker-owncloud-client-win32
+# zypper --non-interactive --gpg-auto-import-keys install sudo
 
 # hack for https://github.com/owncloud/client/issues/5950
 zypper --non-interactive --gpg-auto-import-keys ar http://download.opensuse.org/repositories/isv:/ownCloud:/toolchains:/mingw:/win32:/2.3.3/openSUSE_Leap_42.1/isv:ownCloud:toolchains:mingw:win32:2.3.3.repo
 zypper refresh -f && zypper --non-interactive install mingw32-cross-nsis=3.01
-ln -s ../UAC.dll ../nsProcess.dll /usr/share/nsis/Plugins/x86-ansi/
-ln -s ../UAC.dll ../nsProcess.dll /usr/share/nsis/Plugins/x86-unicode/
+## done in nsis3_compat_hack.sh:
+# ln -s ../UAC.dll ../nsProcess.dll /usr/share/nsis/Plugins/x86-ansi/
+# ln -s ../UAC.dll ../nsProcess.dll /usr/share/nsis/Plugins/x86-unicode/
 # end of hack
  
 sudo -u jenkins $(dirname ${BASH_SOURCE[0]})/compile_client.sh $@
