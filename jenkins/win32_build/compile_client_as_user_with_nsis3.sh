4408f51a4fdb322e8c64e13e8003d6469b509be8
Juergen Weigert <jw@owncloud.com>
2017-12-20 17:53:33 +0100

    pulling mingw32-libopenssl-devel-1.0.2n and mingw32-libopenssl-1.0.2n for https://kanboard.owncloud.com/project/1/task/1772

--- jenkins/win32_build/compile_client_as_user_with_nsis3.sh
+++ jenkins/win32_build/compile_client_as_user_with_nsis3.sh
@@ -13,9 +13,14 @@ zypper mr -d 'OSS Update'
 # Not needed. sudo is already onboard in alfageme/docker-owncloud-client-win32
 # zypper --non-interactive --gpg-auto-import-keys install sudo
 
+zypper --non-interactive --gpg-auto-import-keys ar http://download.opensuse.org/repositories/isv:/ownCloud:/toolchains:/mingw:/win32:/2.3.4/openSUSE_Leap_42.1/isv:ownCloud:toolchains:mingw:win32:2.3.4.repo
+zypper refresh -f
+# hack for https://kanboard.owncloud.com/project/1/task/1772
+zypper --non-interactive install mingw32-libopenssl-devel=1.0.2n mingw32-libopenssl-devel=1.0.2n
+
 # hack for https://github.com/owncloud/client/issues/5950
-zypper --non-interactive --gpg-auto-import-keys ar http://download.opensuse.org/repositories/isv:/ownCloud:/toolchains:/mingw:/win32:/2.3.3/openSUSE_Leap_42.1/isv:ownCloud:toolchains:mingw:win32:2.3.3.repo
-zypper refresh -f && zypper --non-interactive install mingw32-cross-nsis=3.01
+zypper --non-interactive install mingw32-cross-nsis=3.01
+
 ## not no permissions in nsis3_compat_hack.sh:
 cp /usr/share/nsis/Plugins/UAC.dll /usr/share/nsis/Plugins/x86-ansi/
 cp /usr/share/nsis/Plugins/UAC.dll /usr/share/nsis/Plugins/x86-unicode/
