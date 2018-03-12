6bcb6c14a4f1d036a3d639db0f899bede9bd841e
Juergen Weigert <jw@owncloud.com>
2018-03-12 16:41:30 +0100

    protect with timeout 10 against hanging docker inspect commands

--- docker/tools/dockernetio.sh
+++ docker/tools/dockernetio.sh
@@ -8,6 +8,10 @@
 # Usage:
 #  watch -n 9 'dockernetio.sh | persec -c 2 | persec -c 3'
 #
+# Requires:
+#  /usr/bin/timeout from apt-get install coreutils
+#
+# 2018-03-12, jw  protect with timeout 10 against hanging docker inspect commands.
 
 if [ -n "$1" ]; then
   echo "Usage:"
@@ -15,19 +19,24 @@ if [ -n "$1" ]; then
   exit 1
 fi
 
+docker_timeout=10       # docker at lisa hangs when 'docker inspect jenkinsprivate_server_1'
+
 echo "BRIDGE              TX_BYTES     RX_BYTES IFNAME       CONTAINER_NAME"
 
 unknown_if=
 text=
-for c in $(docker ps -q); do
-  nspid=$(docker inspect --format='{{.State.Pid}}' $c)
-  cname=$(docker inspect --format='{{.Name}}' $c | sed -e 's@^/@@')
-  ## nsenter -m runs the ls command or the cat command of the container.
-  ## Elegant, but fails with traefik container, which does not have a populated file system.
-  # devs=$(nsenter -t $nspid -m ls /sys/class/net)
-  ## nsenter -n runs the ip command of the host with the nextwork context if the container. That always works.
-  devs_at=$(nsenter -t $nspid -n ip a | grep @if | sed -e 's@^[0-9]*:\s*@@' -e 's@:.*@@')
-  for dev_at in $devs_at; do
+for c in $(timeout $docker_timeout docker ps -q); do
+  nspid=$(timeout $docker_timeout docker inspect --format='{{.State.Pid}}' $c)
+  cname=$(timeout $docker_timeout docker inspect --format='{{.Name}}' $c | sed -e 's@^/@@')
+  if [ -z "$nspid" ]; then
+    echo "docker inspect $c failed"
+  else
+    ## nsenter -m runs the ls command or the cat command of the container.
+    ## Elegant, but fails with traefik container, which does not have a populated file system.
+    # devs=$(nsenter -t $nspid -m ls /sys/class/net)
+    ## nsenter -n runs the ip command of the host with the nextwork context if the container. That always works.
+    devs_at=$(nsenter -t $nspid -n ip a | grep @if | sed -e 's@^[0-9]*:\s*@@' -e 's@:.*@@')
+    for dev_at in $devs_at; do
       # iflink=$(nsenter -t $nspid -m cat /sys/class/net/$dev/iflink)
       iflink=$(echo $dev_at | sed -e 's/.*@if//')
       ifname=$(grep -l $iflink /sys/class/net/*/ifindex | sed -e 's@^/sys/class/net/@@' -e 's@/ifindex@@')
@@ -40,7 +49,8 @@ for c in $(docker ps -q); do
         # dind?
         unknown_if="$unknown_if $nspid:$dev_at"
       fi
-  done
+    done
+  fi
 done
 echo "$text" | sort	# group by bridge
 test -n "$unknown_if" && echo "unknown interfaces: $unknown_if"
